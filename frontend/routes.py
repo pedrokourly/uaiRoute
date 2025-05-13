ae2985f600c6448cb578ed27498cc52a507b17c8
Pedro Kourly <pedrohenriquekourly@hotmail.com>
2025-05-13 10:40:22 -0300

    Add Flask application structure and templates for vehicle registration

---
+++ frontend/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from uairoute import app
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/obras')
+def obras():
+    return render_template('obras/lista-de-formulario.html')
+
+@app.route('/obras/cadastrar')
+def cadastrarObra():
+    return render_template('obras/cadastrar.html')
